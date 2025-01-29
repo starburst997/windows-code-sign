9cc312bab36c3e56944f86d7ff0578084bf3cc5a
Jean-Denis Boivin <jd@boiv.in>
2025-01-29 17:33:57 -0500

    Test signing

--- CodeSignCpp/CodeSignCpp.cpp
+++ CodeSignCpp/CodeSignCpp.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!!!" << '\n';
+    std::cout << "Hello, World!!!!" << '\n';
     std::cout << "Testing Code Signing..." << '\n';
     std::cin.ignore();
     return 0;
